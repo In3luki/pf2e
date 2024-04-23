2f7e37e596195be36e32ba72e9b1750221261ade
In3luki <41452412+In3luki@users.noreply.github.com>
2024-04-23 23:52:49 +0200

    Fix parsing damage rolls with `MathTerms` and remove deprecated `async` option for roll evaluation (#14510)

--- src/module/item/effect/document.ts
+++ src/module/item/effect/document.ts
@@ -1,12 +1,9 @@
 import type { ActorPF2e } from "@actor";
 import type { BadgeReevaluationEventType, EffectBadge, EffectBadgeSource } from "@item/abstract-effect/data.ts";
 import { AbstractEffectPF2e, EffectBadgeFormulaSource, EffectBadgeValueSource } from "@item/abstract-effect/index.ts";
-import { reduceItemName } from "@item/helpers.ts";
-import { ChatMessagePF2e } from "@module/chat-message/index.ts";
 import type { RuleElementOptions, RuleElementPF2e } from "@module/rules/index.ts";
 import type { UserPF2e } from "@module/user/index.ts";
 import { ErrorPF2e, sluggify } from "@util";
-import * as R from "remeda";
 import type { EffectFlags, EffectSource, EffectSystemData } from "./data.ts";
 
 class EffectPF2e<TParent extends ActorPF2e | null = ActorPF2e | null> extends AbstractEffectPF2e<TParent> {
@@ -120,14 +117,7 @@ class EffectPF2e<TParent extends ActorPF2e | null = ActorPF2e | null> extends Ab
         const actor = this.actor;
         if (!actor) throw ErrorPF2e("A formula badge can only be evaluated if part of an embedded effect");
 
-        const roll = await new Roll(badge.value, this.getRollData()).evaluate({ async: true });
-        const token = actor.getActiveTokens(false, true).shift();
-        const label = badge.labels ? badge.labels?.at(roll.total - 1)?.trim() : null;
-        roll.toMessage({
-            flavor: R.compact([reduceItemName(this.name), label ? `(${label})` : null]).join(" "),
-            speaker: ChatMessagePF2e.getSpeaker({ actor, token }),
-        });
-
+        const roll = await new Roll(badge.value, this.getRollData()).evaluate();
         const initial = initialValue ?? roll.total;
         const reevaluate = badge.reevaluate ? { event: badge.reevaluate, formula: badge.value, initial } : null;
         return { type: "value", value: roll.total, labels: badge.labels, reevaluate };
